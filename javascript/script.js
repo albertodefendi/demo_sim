3b8e7bf21ff94a26e2a384aa30e8e82d5be99e80
Alberto Defendi <133432639+albertodefendi@users.noreply.github.com>
2024-07-28 00:46:07 +0200

    Progresses

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,1 @@
-let navNameContainerWidth = parseFloat(document.getElementById("navbar-name-container").offsetWidth);
-let navButtonContainer = document.getElementById("navbar-button-container");
-navButtonContainer.style.minWidth = `${navNameContainerWidth}px`;
+
