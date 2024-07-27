c40bc7175d77ecd61b23b69bda51d3156f087fbb
Alberto Defendi <133432639+albertodefendi@users.noreply.github.com>
2024-07-27 17:01:58 +0200

    Added Navbar

---
+++ javascript/script.js
@@ -0,0 +1,3 @@
+let navNameContainerWidth = document.getElementById("navbar-name-container").offsetWidth;
+let navButtonContainer = document.getElementById("navbar-button-container");
+navButtonContainer.style.minWidth = `${navNameContainerWidth}px`;
