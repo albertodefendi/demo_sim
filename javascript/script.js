c4d17e1f3da09a0c57d4414cea885b6163681a4d
Alberto Defendi <133432639+albertodefendi@users.noreply.github.com>
2024-08-08 17:23:02 +0200

    Progresses

--- javascript/script.js
+++ javascript/script.js
@@ -1,7 +1,7 @@
 let newsletterButton = document.getElementById("newsletter-subscribe");
 newsletterButton.addEventListener("click", () => {
     document.getElementById("email").value = "";
-    let arrowImg = document.getElementById("arrow-img")
+    let arrowImg = document.getElementById("newsletter-subscribe-button")
     arrowImg.src = "../assets/checkmark.svg";
     setTimeout(() => {
         arrowImg.src = "../assets/arrow.svg";
