6f88d21fe3a5dfa82f87c0c58e0bc5411692d677
Alberto Defendi <133432639+albertodefendi@users.noreply.github.com>
2024-08-28 11:14:44 +0200

    Added dark theme

---
+++ shopping_website/javascript/script.js
@@ -0,0 +1,12 @@
+document.getElementById('dark-mode-toggle').addEventListener('click', function() {
+    if (document.documentElement.getAttribute('data-theme') === 'dark') {
+        document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light');
+    } else {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
+    }
+});
+
+const savedTheme = localStorage.getItem('theme') || (window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light');
+document.documentElement.setAttribute('data-theme', savedTheme);
